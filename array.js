2964e0102ee5cc115e12560b7e2de4e58de52ab5
ashraf-git <aliashraf.hena@gmail.com>
2020-07-23 20:25:22 +0600

    array-indexof-pop-push-unshift-shift

--- array.js
+++ array.js
@@ -1,1 +1,19 @@
-array.js
+var friendAge = [34, 24, 56, 76, 74, 33];
+// var sonaliAge = friendAge[3];
+// friendAge[2] = 80;
+// // console.log(sonaliAge);
+// // console.log(friendAge);
+
+// var position = friendAge.indexOf(74);
+// var position1 = friendAge.indexOf(748);
+// console.log(position)
+// console.log(position1)
+// console.log(friendAge);
+// friendAge.push(35);
+// friendAge.push(86)
+console.log(friendAge.length)
+console.log(friendAge)
+friendAge.pop();
+// friendAge.unshift(12)
+friendAge.shift()
+console.log(friendAge)
